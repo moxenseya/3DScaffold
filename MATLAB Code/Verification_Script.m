df57f7963ffde07860182e9af2ec11a88b9981dd
roxas76927 <keyblademaster1000@gmail.com>
2019-09-16 15:17:21 -0400

    Hardcoded path to disk image

--- MATLAB Code/Verification_Script.m
+++ MATLAB Code/Verification_Script.m
@@ -3,10 +3,12 @@
 %Step 1: Create a volume and initialize it to zeros
 
 %%
-Input_directory_path = uigetdir(pwd,'Select folder Verification Disk');
-Input_directory = dir(strcat(Input_directory_path, '\Disk_Verification.TIF'));
+%Input_directory_path = uigetdir(pwd,'Select folder Verification Disk');
+%Input_directory = dir(strcat(Input_directory_path, '\Disk_Verification.TIF'));
 
-disp (['Selected directory is: ', Input_directory_path]);
+Input_directory = dir('C:\Users\Abrar\Desktop\3DScaffold\Datasets\Disk_Verification.TIF');
+
+%disp (['Selected directory is: ', Input_directory_path]);
 
 
 %%% Initialize the array using zeros based on the size of the images.
@@ -22,15 +24,15 @@ FileName=Input_directory(1).name;
 temp2 =imread(strcat(relativepath(Input_directory_path), FileName));
 disp (FileName)
 diskImg =imread(strcat(relativepath(Input_directory_path), FileName));
-diskImg=diskImg(1:2:end,1:2:end,1);
+diskImg=diskImg(1:4:end,1:4:end,1);
 
 global final_hole_volume
-final_hole_volume=zeros(size(diskImg,1), size(diskImg, 2), round(ceil(150)));
+final_hole_volume=zeros(size(diskImg,1), size(diskImg, 2), round(ceil(256)));
 
 %Step 2: Repeatedly copy and put the image into this volume with imfills
 
 %%
-for i=50:round(100)
+for i=103:153
     %final_hole_volume(:,:,i)=imfill(diskImg,'holes');
     final_hole_volume(:,:,i)=diskImg;
 end
@@ -76,15 +78,15 @@ subplot(223), imshow(mean(final_hole_volume,3),[]);title('Original Volume');
 %Change the values of areavalue and the for loop constraints to make the
 %size change for the arrays.
 tic
-areavalue=zeros(21,21);
+areavalue=zeros(41,41);
 Xvalues = zeros(size(areavalue,1),size(areavalue,2));
 Yvalues = zeros(size(areavalue,1),size(areavalue,2));
-for rotX = -10:1:10
-   for rotY = -10:1:10
-      areavalue(rotX+11,rotY+11)= OptimizeArea_Verification([rotX,rotY,1]);
-      Xvalues(rotX+11,rotY+11)= rotY;
-      Yvalues(rotX+11,rotY+11)= rotY;
-      disp(['iteration ', num2str(rotX+11), ' ', num2str(rotY+11)]);
+for rotX = -20:1:20
+   for rotY = -20:1:20
+      areavalue(rotX+21,rotY+21)= OptimizeArea_Verification([rotX,rotY,1]);
+      Xvalues(rotX+21,rotY+21)= rotY;
+      Yvalues(rotX+21,rotY+21)= rotY;
+      disp(['iteration ', num2str(rotX+21), ' ', num2str(rotY+21)]);
    end
 end    
 toc
@@ -93,12 +95,12 @@ toc
 %Experimental Section : Plotting values on Surf for 3D Plot
 
 
-surf(Xvalues,Yvalues,areavalue)
+figure, surf(Xvalues,Yvalues',areavalue)
 
 
 %Step 5: Apply Optimization to restore rotation 
 %%
-    optionsSA=optimoptions('simulannealbnd','MaxIterations', 10,'PlotFcns',...
+    optionsSA=optimoptions('simulannealbnd','MaxIterations', 100,'PlotFcns',...
         {@saplotbestx,@saplotbestf,@saplotx,@saplotf});
 
     
