1b2c663c76a99e8d47c9263ec6acec958fcb8979
roxas76927 <keyblademaster1000@gmail.com>
2019-09-16 15:41:00 -0400

    Converted hardcoded variables into declarations. Single variable controls everything else

--- MATLAB Code/SurfacePlot.m
+++ MATLAB Code/SurfacePlot.m
@@ -1,16 +1,27 @@
 %%
-%Change the values of areavalue and the for loop constraints to make the
-%size change for the arrays.
+%Change the value of the UB variable only. It controls the upper bound,
+%lower bound, and the X and Y dimnesions of the areas matrix. Since all the
+%values are symmetric around 0, the program calculates the dimesions
+%automatically.
+
+UB=25;
+
+LB=-UB;
+DIM = (UB*2)+1;
+
+disp (['Dimensions of the array are: ', int2str(DIM) , ' x ' , int2str(DIM)]);
+disp(['The bounds for the rotations are: ', int2str(LB) , ' to ' , int2str(UB)]);
+
 tic
-areavalue=zeros(41,41);
-Xvalues = zeros(size(areavalue,1),size(areavalue,2));
-Yvalues = zeros(size(areavalue,1),size(areavalue,2));
-for rotX = -20:1:20
-   for rotY = -20:1:20
-      areavalue(rotX+21,rotY+21)= OptimizeArea_Verification([rotX,rotY,1]);
-      Xvalues(rotX+21,rotY+21)= rotY;
-      Yvalues(rotX+21,rotY+21)= rotY;
-      disp(['iteration ', num2str(rotX+21), ' ', num2str(rotY+21)]);
+areavalue=zeros(DIM,DIM);
+Xvalues = zeros(size(areavalue));
+Yvalues = zeros(size(areavalue));
+for rotX = LB:1:UB
+   for rotY = LB:1:UB
+      areavalue((rotX+UB+1),(rotY+UB+1))= OptimizeArea_Verification([rotX,rotY,1]);
+      Xvalues((rotX+UB+1),(rotY+UB+1))= rotY;
+      Yvalues((rotX+UB+1),(rotY+UB+1))= rotY;
+      disp(['iteration ', num2str(rotX+UB+1), ' ', num2str(rotY+UB+1)]);
    end
 end    
 toc
